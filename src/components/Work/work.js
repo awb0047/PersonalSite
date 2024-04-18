d9a932f9db576f4d80fe689d9b6e3d092a062d9f
awb0047 <add9mob@gmail.com>
2024-04-18 16:39:20 -0500

    Test Change

--- src/components/Work/work.js
+++ src/components/Work/work.js
@@ -15,7 +15,7 @@ export function Work( {
                 <TextCard Title={"COMET PROXIES"} Text={"Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time."} GitLink={"https://github.com/awb0047"} DemoLink={"https://www.amazon.com/"}/>
 
                 <TextCard Title={"PSEUDO"} Text={"Coindom is a crypto app that allows users to search for information about various cryptocurrencies in real-time."} GitLink={"https://github.com/TrevorAupperle/pseudo"} DemoLink={"https://www.amazon.com/"}/>
-                <Hovercard backgroundImage={"pseudo.jpg"} link = "https://www.cometproxies.com/" text = "Pseudo"/>
+                <Hovercard backgroundImage={"pseudo.jpg"} link = "https://github.com/TrevorAupperle/pseudo" text = "Pseudo"/>
 
                 <Hovercard backgroundImage={"entertainment.png"} link = "https://lkqentertainment.com/" text = "LKQ Entertainment"/>
                 <TextCard Title={"LKQ ENTERTAINMENT"} Text={"In the realm of code, logic unfolds like a choreographed dance, where variables and functions perform in a symphony of precision. Programmers craft stories of algorithms, pushing boundaries in the ever-evolving dance of technology."} GitLink={"https://github.com/awb0047"} DemoLink={"https://lkqentertainment.com/"}/>
